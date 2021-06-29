0e0b2997f5fb1a7c3b392169ddeba775384eb448
Samuel FURLONG <samuel.furlong@students.emmauscc.sa.edu.au>
2021-06-29 12:49:35 +0930

    Login function mostly complete.

--- login.js
+++ login.js
@@ -1,10 +1,34 @@
-function userLogin(key){
+$(document).ready(function(){
 
-    var getKeyInfo = firebase.database().ref(key);
-
-    getKeyInfo.on('value', (snapshot) => {
-        const tempperdata = snapshot.val();
-        perdata = JSON.parse(tempperdata);
+    $('.loginButton').click(console.log('pressed'));
+    
+    $('.loginButton').click(loginUser());
+        
+    function loginUser(){
+        console.log("button pressed");
+        console.log($('.IDinput').val());
+        firebaseLogin($('.IDinput').val());
         console.log(perdata);    
-    })
-}
+        if (perdata==null){
+            $('.alertbox').html('Login failed. Try again');
+        }
+        if (perdata!=null){
+            //window.location.href = "index.html";
+        }
+    };
+
+
+    function firebaseLogin(key){
+
+        /*
+        var getKeyInfo = firebase.database().ref(key);
+
+        getKeyInfo.on('value', (snapshot) => {
+            const tempperdata = snapshot.val();
+            perdata = JSON.parse(tempperdata);
+            console.log(perdata);    
+        })
+        */
+    }
+
+});
