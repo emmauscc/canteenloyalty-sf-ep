ad307861006bef917e36fa8e3199d3463ee3456c
Samuel FURLONG <samuel.furlong@students.emmauscc.sa.edu.au>
2021-07-27 11:39:17 +0930

    Firbease implemented into login

--- login.js
+++ login.js
@@ -1,19 +1,33 @@
 $(document).ready(function(){
 
+    var firebaseConfig = {
+        apiKey: "AIzaSyAD6I8LABP6IgRjECl6ktRP_xRUajcFQM8",
+        authDomain: "canteen-loyalty.firebaseapp.com",
+        databaseURL: "https://canteen-loyalty-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "canteen-loyalty",
+        storageBucket: "canteen-loyalty.appspot.com",
+        messagingSenderId: "721137803742",
+        appId: "1:721137803742:web:d1ee73eec75fba91673d4f",
+        measurementId: "G-T6VBLGTYVT"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+
     $('.loginButton').click(console.log('pressed'));
     
     $('.loginButton').click(loginUser());
         
     function loginUser(){
         console.log("button pressed");
-        console.log($('.IDinput').val());
-        firebaseLogin($('.IDinput').val());
-        console.log(perdata);    
-        if (perdata==null){
+        console.log($('.IDinput').val());   
+        if ($('.IDinput').val().length<5||$('.IDinput').val().length>5){
             $('.alertbox').html('Login failed. Try again');
-        }
-        if (perdata!=null){
-            //window.location.href = "index.html";
+        } else {
+            console.log('not empty');
+            firebaseLogin($('.IDinput').val());
+            console.log(perdata); 
+            // window.location.href = "index.html";
         }
     };
 
@@ -21,7 +35,7 @@ $(document).ready(function(){
 
     function firebaseLogin(key){
 
-        /*
+        
         var getKeyInfo = firebase.database().ref(key);
 
         getKeyInfo.on('value', (snapshot) => {
@@ -29,7 +43,6 @@ $(document).ready(function(){
             perdata = JSON.parse(tempperdata);
             console.log(perdata);    
         })
-        */
     }
 
 });
