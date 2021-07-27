4af2c9a2a5846434bb5256a08bfd631b28b5cc95
Samuel FURLONG <samuel.furlong@students.emmauscc.sa.edu.au>
2021-07-27 12:42:36 +0930

    Login.js cleaned up

--- login.js
+++ login.js
@@ -1,32 +1,26 @@
-$(document).ready(function(){
-
-    
-        
-    function loginUser(){
-        console.log("button pressed");
-        console.log($('.IDinput').val().length);   
-        if ($('.IDinput').val().length<5||$('.IDinput').val().length>5){
-            $('.errormessage').html('Login failed. Try again');
-        } else {
-            console.log('not empty');
-            firebaseLogin($('.IDinput').val());
-            console.log(perdata); 
-            // window.location.href = "index.html"
-        }
-    };
-
-    
-
-    function firebaseLogin(key){
-
-        
-        var getKeyInfo = firebase.database().ref(key);
-
-        getKeyInfo.on('value', (snapshot) => {
-            const tempperdata = snapshot.val();
-            perdata = JSON.parse(tempperdata);
-            console.log(perdata);    
-        })
+function loginUser(){
+    console.log("button pressed");
+    console.log($('.IDinput').val().length);   
+    if ($('.IDinput').val().length<5||$('.IDinput').val().length>5){
+        $('.errormessage').html('Login failed. Try again');
+    } else {
+        console.log('not empty');
+        firebaseLogin($('.IDinput').val());
+        console.log(perdata); 
+        // window.location.href = "index.html"
     }
+};
 
-});
+
+
+function firebaseLogin(key){
+
+    
+    var getKeyInfo = firebase.database().ref(key);
+
+    getKeyInfo.on('value', (snapshot) => {
+        const tempperdata = snapshot.val();
+        perdata = JSON.parse(tempperdata);
+        console.log(perdata);    
+    })
+};
